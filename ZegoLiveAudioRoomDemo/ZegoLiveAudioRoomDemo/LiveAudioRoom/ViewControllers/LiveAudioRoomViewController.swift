3b93a2b40e19cb71c527e3f9b2b40a79daea5368
Kael Ding <415676243@qq.com>
2021-12-20 15:56:46 +0800

    🎨 Optimize audo room vc

--- ZegoLiveAudioRoomDemo/ZegoLiveAudioRoomDemo/LiveAudioRoom/ViewControllers/LiveAudioRoomViewController.swift
+++ ZegoLiveAudioRoomDemo/ZegoLiveAudioRoomDemo/LiveAudioRoom/ViewControllers/LiveAudioRoomViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import ZIM
 
-class LiveAudioRoomViewController: UIViewController, RoomServiceDelegate, UserServiceDelegate, SpeakerSeatServiceDelegate,InputTextViewDelegate {
+class LiveAudioRoomViewController: UIViewController {
     
     @IBOutlet weak var bottomBar: UIView!
     
@@ -31,76 +31,39 @@ class LiveAudioRoomViewController: UIViewController, RoomServiceDelegate, UserSe
     @IBOutlet weak var memberButtonTrail: NSLayoutConstraint!
     @IBOutlet weak var messageHeightConstraint: NSLayoutConstraint!
     
-    var _inputTextView:InputTextView?
-    var inputTextView:InputTextView? {
-        get {
-            if _inputTextView == nil {
-                _inputTextView = UINib.init(nibName: "InputTextView", bundle: nil).instantiate(withOwner: self, options: nil).last as? InputTextView
-                _inputTextView?.frame = CGRect.init(x: 0, y: self.view.bounds.size.height, width: self.view.bounds.size.width, height: 55)
-                _inputTextView?.delegate = self as InputTextViewDelegate
-            }
-            return _inputTextView
-        }
-    }
+    lazy var inputTextView: InputTextView? = {
+        let textView: InputTextView = UINib(nibName: "InputTextView", bundle: nil).instantiate(withOwner: self, options: nil).last as! InputTextView
+        textView.frame = CGRect(x: 0, y: self.view.bounds.size.height, width: self.view.bounds.size.width, height: 55)
+        textView.delegate = self
+        return textView
+    }()
     
-    var _messageList:Array<LiveAudioMessageModel>?
-    var messageList:Array<LiveAudioMessageModel>? {
-        get {
-            if _messageList == nil {
-                _messageList = Array()
-            }
-            return _messageList
-        }
-        set {
-            _messageList = newValue
-        }
-    }
-    var onseatUserList:Array<Any>?
-    var isMuteAllMessage:Bool = false
-    var localUserID:String?
-    var currentUserInfo:UserInfo?
-    var micAuthorizationTimer:DispatchSourceTimer?
+    var messageList: [LiveAudioMessageModel] = []
+    var isMuteAllMessage: Bool = false
+    var localUserID: String?
+    var currentUserInfo: UserInfo?
+    var micAuthorizationTimer: DispatchSourceTimer?
     
-    deinit {
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyBoardDidShow(node:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyBoardDidHide(node:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
-    }
-    
-    func RegisterServiceCallback() -> Void {
-        RoomManager.shared.roomService.delegate = self
-        RoomManager.shared.userService.addUserServiceDelegate(self)
-        RoomManager.shared.speakerService.delegate = self
-    }
-    
-    //MARK: -SpeakerSeatServiceDelegate
-    func speakerSeatUpdate(_ models: [SpeakerSeatModel]) {
+        RegisterServiceCallback()
         
-    }
-    
-    //MARK: -UserServiceDelegate
-    func connectionStateChanged(_ state: ZIMConnectionState, _ event: ZIMConnectionEvent) {
+        if localUserIsHost() {
+            RoomManager.shared.speakerService.takeSeat(0, callback:nil)
+        }
         
-    }
+        configUI()
 
-    func roomUserJoin(_ users: [UserInfo]) {
-        
-    }
-    
-    func roomUserLeave(_ users: [UserInfo]) {
-        
-    }
-    
-    func receiveTakeSeatInvitation() {
-        
-    }
-    
-    //MARK: -RoomServiceDelegate
-    func receiveRoomInfoUpdate(_ info: RoomInfo?) {
-        
-    }
-    
-    //MARK: -InputTextViewDelegate
-    func inputTextViewDidClickSend(_ message: String?) {
-        
+        if let myself = RoomManager.shared.userService.localInfo {
+            let model: LiveAudioMessageModel = LiveAudioMessageModelBuilder.buildJoinMessageModelWithUser(user: myself)
+            messageList.append(model)
+            reloadMessageData()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -113,76 +76,64 @@ class LiveAudioRoomViewController: UIViewController, RoomServiceDelegate, UserSe
         self.navigationController?.isNavigationBarHidden = false
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyBoardDidShow(node:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyBoardDidHide(node:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        RegisterServiceCallback()
-        
-        if localUserIsHost() {
-            //RoomManager.shared.speakerService.takeSeat(0, callback:nil)
-        }
-        
-        configUI()
-
-        let myself:UserInfo? = RoomManager.shared.userService.localInfo ?? nil
-        if myself != nil {
-            let model:LiveAudioMessageModel = LiveAudioMessageModelBuilder.buildJoinMessageModelWithUser(user: myself!)
-            messageList?.append(model)
-            reloadMessageData()
-        }
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         getKeyWindow().endEditing(true)
     }
     
-    func reloadMessageData() -> Void {
-        updateMessageHeightConstraint()
-        messageView.reloadWithData(data: messageList ?? [])
-        messageView.scrollToBottom()
-    }
+    // MARK: - UI
     
-    func updateMessageHeightConstraint() -> Void {
-        var height:CGFloat = 0
-        for model:LiveAudioMessageModel in messageList! {
-            height += (model.messageHeight ?? 0) + 10*2 + 10
-        }
-        messageHeightConstraint.constant = height
-    }
-    
-    @objc func keyBoardDidShow(node : Notification){
-        let keyboardRect:CGRect = (node.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        
-        let duration:Double = node.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
-        UIView.animate(withDuration: duration) {
-            self.inputView?.frame = CGRect.init(x: 0, y: UIScreen.main.bounds.size.height - keyboardRect.size.height - 55, width: self.view.bounds.size.width, height: 55)
-        }
-    }
-    
-    @objc func keyBoardDidHide(node : Notification){
-        UIView.animate(withDuration: 0.25) {
-            self.inputView?.frame = CGRect.init(x: 0, y: UIScreen.main.bounds.size.height, width: self.view.bounds.size.width, height: 55)
-        }
-    }
-    
-    func localUserIsHost() -> Bool {
-        return RoomManager.shared.userService.localInfo?.userID == RoomManager.shared.roomService.info.hostID
+    func RegisterServiceCallback() -> Void {
+        RoomManager.shared.roomService.delegate = self
+        RoomManager.shared.userService.addUserServiceDelegate(self)
+        RoomManager.shared.speakerService.delegate = self
     }
     
     func configUI() -> Void {
         roomTitleLabel.text = RoomManager.shared.roomService.info.roomName
         roomIdLabel.text = RoomManager.shared.roomService.info.roomID
+    }
+    
+    func reloadMessageData() -> Void {
+        updateMessageHeightConstraint()
+        messageView.reloadWithData(data: messageList)
+        messageView.scrollToBottom()
+    }
+    
+    func updateMessageHeightConstraint() -> Void {
+        var height:CGFloat = 0
+        for model:LiveAudioMessageModel in messageList {
+            height += (model.messageHeight ?? 0) + 10*2 + 10
+        }
+        messageHeightConstraint.constant = height
+    }
+    
+    // MARK: - Notification
+    @objc func keyBoardDidShow(node : Notification){
         
+        guard let userInfo = node.userInfo else { return }
+        guard let keyboardValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardRect: CGRect = keyboardValue.cgRectValue
+        
+        guard let durationValue = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber else { return }
+        let duration: Double = durationValue.doubleValue
+        
+        UIView.animate(withDuration: duration) {
+            self.inputView?.frame = CGRect(x: 0,
+                                           y: UIScreen.main.bounds.size.height - keyboardRect.size.height - 55,
+                                           width: self.view.bounds.size.width,
+                                           height: 55)
+        }
+    }
+    
+    @objc func keyBoardDidHide(node : Notification){
+        UIView.animate(withDuration: 0.25) {
+            self.inputView?.frame = CGRect(x: 0,
+                                           y: UIScreen.main.bounds.size.height,
+                                           width: self.view.bounds.size.width,
+                                           height: 55)
+        }
     }
     
     //MARK: - Action
@@ -204,7 +155,54 @@ class LiveAudioRoomViewController: UIViewController, RoomServiceDelegate, UserSe
     
     
     @IBAction func messageButtonClick(_ sender: UIButton) {
+        
+    }
+}
+
+// MARK: - Private
+extension LiveAudioRoomViewController {
+    func localUserIsHost() -> Bool {
+        return RoomManager.shared.userService.localInfo?.userID == RoomManager.shared.roomService.info.hostID
+    }
+}
+
+extension LiveAudioRoomViewController : InputTextViewDelegate {
+    //MARK: -InputTextViewDelegate
+    func inputTextViewDidClickSend(_ message: String?) {
+        
+    }
+}
+
+extension LiveAudioRoomViewController : RoomServiceDelegate {
+    //MARK: -RoomServiceDelegate
+    func receiveRoomInfoUpdate(_ info: RoomInfo?) {
+        
+    }
+}
+
+extension LiveAudioRoomViewController : UserServiceDelegate {
+    //MARK: -UserServiceDelegate
+    func connectionStateChanged(_ state: ZIMConnectionState, _ event: ZIMConnectionEvent) {
+        
+    }
+
+    func roomUserJoin(_ users: [UserInfo]) {
+        
     }
     
-
+    func roomUserLeave(_ users: [UserInfo]) {
+        
+    }
+    
+    func receiveTakeSeatInvitation() {
+        
+    }
 }
+
+extension LiveAudioRoomViewController : SpeakerSeatServiceDelegate {
+    //MARK: -SpeakerSeatServiceDelegate
+    func speakerSeatUpdate(_ models: [SpeakerSeatModel]) {
+        
+    }
+}
+
