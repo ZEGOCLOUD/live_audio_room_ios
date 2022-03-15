6af92c768d7270adcadc2281db1982944610388f
席航 <careyxi@zego.im>
2022-03-15 11:11:15 +0800

    📝: change ReadMe.

--- ZegoLiveAudioRoomDemo/ZegoLiveAudioRoom/RoomManager.swift
+++ ZegoLiveAudioRoomDemo/ZegoLiveAudioRoom/RoomManager.swift
@@ -106,7 +106,7 @@ class RoomManager: NSObject {
 }
 
 extension RoomManager {
-    func loginRtcRoom(with rtcToken: String) {
+    func loginRtcRoom(with token: String) {
         guard let userID = RoomManager.shared.userService.localInfo?.userID else {
             assert(false, "user id can't be nil.")
             return
@@ -121,7 +121,7 @@ extension RoomManager {
         let user = ZegoUser(userID: userID)
         
         let config = ZegoRoomConfig()
-        config.token = rtcToken
+        config.token = token
         config.maxMemberCount = 0
         ZegoExpressEngine.shared().loginRoom(roomID, user: user, config: config)
         
